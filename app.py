f627c05af665428f67d4e01ba24b3d9d19e1bf85
Monita <mitramonita@gmail.com>
2025-01-03 16:29:27 -0600

    set up basic python env and configured api keys

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, request
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Set Slack API credentials
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+
+# Initialize the Slack app
+app = App(token=SLACK_BOT_TOKEN)
+
+# Initialize the Flask app
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+def get_bot_user_id():
+    """
+    Get the bot user ID using the Slack API
+    Returns:
+        str: The bot user ID
+    """
+    try:
+        # initialize Slack client with bot token
+        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        response = slack_client.auth_test()
+        return response["user_id"]
+    except SlackApiError as e:
+        print(f"Error: {e}")
